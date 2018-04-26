e52256dde94a3142bf393240db8c33fe56d3c608
Alexandre Ximenes <xyymenes@gmail.com>
2018-04-26 16:50:13 -0300

    refatorando codigo com function, adicionando classes via jQuery

--- Treinamento jQuery I/public/js/main.js
+++ Treinamento jQuery I/public/js/main.js
@@ -1,36 +1,59 @@
-let _frase = $('.frase').text();
-let _numPalavras = _frase.split(" ").length;
+let _tempoInicial = $("#tempo-digitacao").text();
+let _textarea = $(".textarea-digitacao");    
 
-let _tamanhoFrase = $("#tamanho-frase").text(_numPalavras);
-
-let _textarea = $(".textarea-digitacao");
-
-// evento de caractere e palavras
-_textarea.on('input', () => {
-    
-    let _textoDigitado = _textarea.val();
-    let _qtdPalavras = _textoDigitado.split(/\S+/).length -1;
-    $("#contador-palavras").text(_qtdPalavras);
-    $("#contador-caracteres").text(_textoDigitado.length);
-
+//ECMA5
+$(  () => {
+        atualizaTamanhoFrase();
+        inicializaContadores();
+        inicializaCronometro();
+        $("#botao-reiniciar").click(reiniciaJogo);
 });
+
+function atualizaTamanhoFrase() {
+    let _frase = $(".frase").text();
+    let _numPalavras = _frase.split(" ").length;
+    let _tamanhoFrase = $("#tamanho-frase").text(_numPalavras);
+}
+
+function inicializaContadores() {
+    // evento de caractere e palavras
+    _textarea.on('input', () => {
+        let _textoDigitado = _textarea.val();
+        let _qtdPalavras = _textoDigitado.split(/\S+/).length -1;
+        $("#contador-palavras").text(_qtdPalavras);
+        $("#contador-caracteres").text(_textoDigitado.length);
+
+    });
+}
 
 //Cronometro 
-let _tempoRestante = $("#tempo-digitacao").text();
-_textarea.one('focus', () => {
-    let _tempo = setInterval( () => {
-        console.log(_tempoRestante--);
-        $("#tempo-digitacao").text(_tempoRestante);
+function inicializaCronometro() {
+    let _tempoRestante = $("#tempo-digitacao").text();
+    _textarea.one('focus', () => {
+        let _tempo = setInterval( () => {
+            console.log(_tempoRestante--);
+            $("#tempo-digitacao").text(_tempoRestante);
+            if(_tempoRestante < 1) {
+                _textarea.attr("disabled", true); 
+                clearInterval(_tempo);
+                //_textarea.css('background-color', 'lightgray');
+                _textarea.addClass('campo-desativado');
+            }
+        }, 1000);
+    });    
+}
 
-        if(_tempoRestante < 1) {
-            _textarea.attr("disabled", true); 
-            clearInterval(_tempo);
-        }
-    }, 1000);
-});
+function reiniciaJogo() {
+    _textarea.attr("disabled", false);
+    _textarea.val("");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
+    $("#tempo-digitacao").text(_tempoInicial);
+    inicializaCronometro();
+     _textarea.removeClass("campo-desativado");
+}
 
-_textarea.on("blur", () => {
-  console.log("blur");
-});
+
+_textarea.blur( () => console.log("blur") );
 
 
