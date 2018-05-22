33fb3d1cdfd606d375d3cfc34dfc4172f22b3db6
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-21 23:49:55 -0300

    fazendo experiencias com javascript puro e com jquery

--- carrinho_compras/js/mScript.js
+++ carrinho_compras/js/mScript.js
@@ -1,4 +1,25 @@
+let valorTotal = [];
+
+document.querySelectorAll('.item-total')
+        .forEach(item => valorTotal.push( parseFloat(item.textContent) ));
+
+let mTotal = valorTotal.reduce((total, i) => total + i, 0.0);
+
+console.log("com Vanila Javascript : " + mTotal);
+
+
 $(function () {
-    let itemTotal = $('.item-total').text();
-    console.log(itemTotal);
+    let total = 0;
+    let itemTotal = $('.item-total');
+    
+    for(var i = 0;i < itemTotal.length; i++){
+        total += parseFloat( $(itemTotal[i]).text() );
+    }
+    //console.log(total);
+    
+    //Setando na pagina resultado
+    $('#valor-total').text(total);
+    $('#quantidade-de-itens').text(itemTotal.length);
+
+
 });
