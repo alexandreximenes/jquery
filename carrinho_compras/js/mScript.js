efad878f44ba13a97263ed0366468c4a2ef76ef3
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-22 00:36:47 -0300

    usando funcoes parseFloat/int, closest, find, text, this, remove

--- carrinho_compras/js/mScript.js
+++ carrinho_compras/js/mScript.js
@@ -9,17 +9,42 @@ console.log("com Vanila Javascript : " + mTotal);
 
 
 $(function () {
-    let total = 0;
-    let itemTotal = $('.item-total');
-    
-    for(var i = 0;i < itemTotal.length; i++){
-        total += parseFloat( $(itemTotal[i]).text() );
+    function totaliza(){
+        let total = 0;
+        let itemTotal = $('.item-total');
+
+        for (var i = 0; i < itemTotal.length; i++) {
+            total += parseFloat($(itemTotal[i]).text());
+        }
+        //console.log(total);
+
+        //Setando na pagina resultado
+        $('#valor-total').text(total);
+        $('#quantidade-de-itens').text(itemTotal.length);
     }
-    //console.log(total);
-    
-    //Setando na pagina resultado
-    $('#valor-total').text(total);
-    $('#quantidade-de-itens').text(itemTotal.length);
+
+    $('.remover').click(function(e) {
+        e.preventDefault();
+        
+        var novaQuantidade = parseInt($("#quantidade-de-itens").text()) -1;
+
+        if(novaQuantidade >= 0) $("#quantidade-de-itens").text(novaQuantidade);
+
+        var self = $(this);
+        var tr = self.closest('tr');
+        var valor = tr.find('.item-total').text();
+
+        var valorTotal = $("#valor-total").text();
+        valorTotal -= valor;
+        $("#valor-total").text(valorTotal);
+        console.log(valorTotal);
+        tr.remove();
+        
+
+
+    });
+
+    totaliza();
 
 
 });
